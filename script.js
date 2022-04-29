3fe6b2552306a5498c64edaa8d4d418e182139ab
NeYaroslav <91835506+NeYaroslav@users.noreply.github.com>
2022-04-29 15:24:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(() => {
+    var dataString = new Date();
+    let result = `${dataString}`
+    let dataArray = result.split(' ')
+    // console.log(dataArray)
+    let currentTime = dataArray[4]
+    // console.log(currentTime)
+    let currentTimeArray = currentTime.split(':')
+    // console.log(currentTimeArray)
+    let hourArrow = currentTimeArray[0] > 12? (Number(currentTimeArray[0]) - 12) * 60 + Number(currentTimeArray[1])
+    :Number(currentTimeArray[0]) * 60 + Number(currentTimeArray[1])
+
+    let minuteArrow = (Number(currentTimeArray[1]) * 60) + Number(currentTimeArray[2])
+
+    console.log(`${minuteArrow} and ${hourArrow}`)
+
+    let hourArrowDOM = document.querySelector(".hour__arrow")
+    hourArrowDOM.style.transform = `rotate(${hourArrow * 0.5}deg)`;
+
+    let minuteArrowDOM = document.querySelector(".minute__arrow")
+    minuteArrowDOM.style.transform = `rotate(${minuteArrow * 0.1}deg)`;
+}, 1000);
